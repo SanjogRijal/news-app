b51d335396dba60ba05a43e62585aaa5139d66fe
V <sanjogrijal@hotmail.co.uk>
2024-05-26 19:59:25 +0545

    feat: change folder structure

---
+++ src/app/components/constants/constants.ts
@@ -0,0 +1,6 @@
+export const TOP_HEADLINES_API = '/top-headlines/category';
+export const CNN_HEADLINES_API = '/everything/cnn.json';
+export const ABC_NEWS_HEADLINES_API = '/everything/abcnews.json'
+export const TECHNOLOGY_NEWS_API = '/technology/us.json';
+export const DESIGN_NEWS_API = '/design/us.json';
+export const HOMEPAGE_API = `${CNN_HEADLINES_API}`;
